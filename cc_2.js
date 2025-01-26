dd02d64a0140aeec5c6e39eb9f97c317dd850500
danielaj2004 <danielajoya12@gmail.com>
2025-01-25 22:36:30 -0500

    Task 1- store inventory

---
+++ cc_2.js
@@ -0,0 +1,9 @@
+// Task 1: Working with arrays //
+Let products = ["Toothbrush", "toothpaste". "mouthwash". "floss", "gum"];  // Array of products
+
+products.push("electric toothbrush");  // Add new product to the end of the array
+console.log(products);  // Print the array of products
+
+products.pop();  // Remove the last product from the array
+console.log("Updated products", products)  // Print the array of products
+
