aa51d69f31a21978667545480991a95f88bbd9ae
danielaj2004 <danielajoya12@gmail.com>
2025-01-25 22:52:40 -0500

    Task 2- student scores

--- cc_2.js
+++ cc_2.js
@@ -7,3 +7,12 @@ console.log(products);  // Print the array of products
 products.pop();  // Remove the last product from the array
 console.log("Updated products", products)  // Print the array of products
 
+// Task 2: Accessung and Modifying Arrays
+Let scores = [58,29,62,72,96];  // Array of scores
+scores[1] = 62;  // change the second number of the array
+Let total = scores.reduce((a, b) => a + b, 0);  // calculate the total of the scores
+Let average = total / scores.length;  // calculate the average of the scores
+
+console.log("Updated scores", scores);  // print the updated array of scores
+console.log(total);  // Print the sum of the scores
+console.log("average score", average);  // print the average of the scores
